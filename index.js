6bc917a48597f79dadab18b6f1f1d01b1f5c56db
Ganbayar Gansukh <ganbayar.gansukh@kellpro.com>
2021-03-13 23:23:08 -0600

    added logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const logger = require('./middleware/logger')
 
 // routes
 const venuesRoutes = require('./routes/venues')
@@ -7,11 +8,14 @@ const venuesRoutes = require('./routes/venues')
 dotenv.config({path: './config/config.env'})
 
 const app = express()
+
+// middleware
+app.use(logger)
+
+// route
 app.use('/v1/venues', venuesRoutes)
+
 const port = process.env.PORT || 5000
-
-
-
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port:${process.env.PORT}`)
 })
