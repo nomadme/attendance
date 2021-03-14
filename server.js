d75ba142702047eab1d3bba6f351033e4326c1b8
Ganbayar Gansukh <ganbayar.gansukh@kellpro.com>
2021-03-13 20:51:53 -0600

    sample response

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http')
+
+const venue = [
+  {id:1, name: "Dallas Convention Center"}
+]
+
 const server = http.createServer((req, res) => {
   const {headers, url, method} = req
-  res.end()
+  res.setHeader('Content-Type', 'application/json')
+  res.end(
+    JSON.stringify({
+      success:true,
+      data: venue
+    })
+  )
 })
 
 const PORT = 5000
