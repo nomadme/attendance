009023c77730cf60902d20fd249c2ff2f3015cab
Ganbayar Gansukh <ganbayar.gansukh@kellpro.com>
2021-03-13 22:35:16 -0600

    set basic routes

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ dotenv.config({path: './config/config.env'})
 const app = express()
 const port = process.env.PORT || 5000
 
-// app.get('/', (req, res) => {
-//   res.send("Get the attendance list")
-// })
+app.get('/v1/venues', (req, res) => {
+  res.status(200).json({success: true, message: 'Listing all venues', data: []})
+})
+
+app.get('/v1/venues/:id', (req, res) => {
+  res.status(200).json({success: true, message: `Listing venue id: ${req.params.id}`, data: []})
+})
+
+app.post('/v1/venues', (req, res) => {
+  res.status(200).json({success: true, message: 'Created venue'})
+})
+
+app.put('/v1/venues/:id', (req, res) => {
+  res.status(200).json({success: true, message: `Updating venue id: ${req.params.id}`, data: []})
+})
+
+app.delete('/v1/venues/:id', (req, res) => {
+  res.status(200).json({success: true, message: `Deleting venue id: ${req.params.id}`, data: []})
+})
 
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port:${process.env.PORT}`)
