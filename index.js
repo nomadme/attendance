3a72bd28df951842f68708daa5e95d8411294c0f
Ganbayar Gansukh <ganbayar.gansukh@kellpro.com>
2021-03-14 10:49:12 -0500

    added mongo db and mongoos for database abstraction

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 // routes
 const venuesRoutes = require('./routes/venues')
 
+// load environment variables
 dotenv.config({path: './config/config.env'})
+
+// connect to database
+connectDB()
 
 const app = express()
 
@@ -18,6 +24,13 @@ if(process.env.NODE_ENV === 'development') {
 app.use('/v1/venues', venuesRoutes)
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port:${process.env.PORT}`)
+const server = app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port:${process.env.PORT}`.yellow.bold)
+})
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // close server and exit process
+  server.close(() => process.exit(1))
 })
