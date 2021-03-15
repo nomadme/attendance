819b8b1269ce2f19eb18901bce53a96181e96ce3
Ganbayar Gansukh <ganbayar.gansukh@kellpro.com>
2021-03-14 23:37:33 -0500

    create slug from name on the model level

--- models/Venue.js
+++ models/Venue.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const VenueSchema = new mongoose.Schema({
   name: {type: String, required: [true, 'Enter name'], unique: true, trim: true},
@@ -61,5 +62,11 @@ const VenueSchema = new mongoose.Schema({
     default: 'no-photo.jpg'
   }
 }, {timestamps: true})
+
+// Create slug from the name
+VenueSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {lower: true})
+  next()
+})
 
 module.exports = mongoose.model('Venue', VenueSchema)
